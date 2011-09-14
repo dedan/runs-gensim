68c6b7a5de97f3c65bbc0eae764f1d6c75f73356
Stephan Gabler <stephan.gabler@gmail.com>
2011-09-14 11:38:53 +0200

    create also a reference.queries file
    in the old dataset I had a bunch of text files containing words and a reference.queries file containing the human ratings. This time jose gave me all the data in csv files and this script converts the csv to the old data structure.

--- scripts/csv2query.py
+++ scripts/csv2query.py
@@ -1,17 +1,27 @@
 
-
+import os
 import csv
 import glob
 
-path = '/Users/dedan/projects/mpi/data/corpora/sparql/gsquared_new/*.csv'
+path = '/Users/dedan/projects/mpi/data/corpora/sparql/gsquared_new_csvs/'
 
-for fname in glob.glob(path):
+word_id = {}
+with open('/Users/dedan/projects/mpi/data/corpora/sparql/id_word.txt') as f:
+    for line in f.readlines():
+        idx, word = line.strip().split('\t')
+        word_id[word] = idx
+
+
+ref = open(path + 'reference.queries', 'w')
+for fname in glob.glob(path + '*.csv'):
+    wordid = word_id[os.path.basename(fname)[:-4]]
     print fname
     table = csv.reader(open(fname), delimiter=',')
     out = open(fname[:-3]+'txt', 'w')
     print fname[:-3]+'txt'
     for row in table:
         print row[0]
-        out.write(row[0] +'\n')
+        out.write(row[0].replace(' ', '_') +'\n')
+        ref.write(wordid + ' 0 0 ' + row[0].replace(' ', '_') + " " + row[1] + '\n')
     out.close()
 
